789da909b5d0422a7ed3743051d4aacccb320713
Хван <xvan4ik_s-92@bk.ru>
2015-04-22 15:58:42 +0700

    Новые файлы

--- hypercube.r
+++ hypercube.r
@@ -1,12 +1,12 @@
 setwd("/home/user/Документы/ECM-312")
-mytable <- read.table("China.txt", header=TRUE, sep=",")
+mytable <- read.table("Somali.csv", header=TRUE, sep=",")
 years <- c(mytable[1:16,1],"sum")
 chars <- c("Терроризм","Террорист","Оккупация","Наркотики","Насилие","Демократия","Развитие")
 countries <- c("Казахстан","Индия","Армения","Китай","Азербайджан","Туркменистан","Сомали")
 alltable <- array(0,c(17,7,7),list(years,chars,countries))
 attach(mytable)
-vars <- c("Казахстан","Индия","Армения","China.txt","Azerbaijan.txt","Туркменистан","Сомали")
-for(s in c(4,5)){
+vars <- c("Казахстан","Индия","Армения","China.txt","Azerbaijan.txt","Туркменистан","Somali.csv")
+for(s in c(4,5,7)){
   mytable <- read.table(vars[s], header=TRUE, sep=",")
   ymax <- max(mytable[][2:8])
   for(y in 1:16){
@@ -18,14 +18,14 @@ for (v in 1:7){
   alltable[y+1,v,s] <-sum(mytable[,v+1])
 }  
  }
-alltable[,,4]
+alltable[,,7]
 
 supermax <- max(alltable)
 sums <- alltable[17,,]
 colsv <- rainbow(8)
 for(s in 1:7){
-  if(sums[1,5] > 0){
-    cairo_pdf(filename=paste0(countries[s],".pdf"),width=10,heigh=6,onefile=TRUE,pointsize=12,family="serf")
+  if(sums[1,s] > 0){
+    cairo_pdf(filename=paste0(countries[s],"_сум.pdf"),width=10,heigh=6,onefile=TRUE,pointsize=12,family="serf")
     barplot (sums[,s],xlab="Характеристики", ylab="Количество упоминаний",col=colsv,main=paste0("Частота упоминаний характеристик для страны ", countries[s]))
     dev.off()
   } 
